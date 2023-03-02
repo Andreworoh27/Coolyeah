f8f4639cd39578816d60120ff48bc9648c67c17f
Andrew Oroh <andreworoh.ao@gmail.com>
2023-03-02 09:26:57 +0700

    Create Table in database and create seeder
    user, course, course session , user's course

--- database/migrations/2023_02_28_110622_create_courses_table.php
+++ database/migrations/2023_02_28_110622_create_courses_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id('course_id');
+            $table->string('course_name');
+            $table->string('course_description', 500);
+            $table->timestamps();
         });
     }
 
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('courses');
     }
 };
