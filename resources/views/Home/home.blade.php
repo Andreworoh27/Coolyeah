78f047c8c93e9ad216895abb193dca2e9e2be1ac
Andrew Oroh <andreworoh.ao@gmail.com>
2023-05-19 19:06:22 +0700

    Create Auto Playing Caraosel

--- resources/views/Home/home.blade.php
+++ resources/views/Home/home.blade.php
@@ -3,15 +3,33 @@
 @section('title', 'Home')
 
 @section('content')
-    {{-- {{ dd(Auth::user()->name) }} --}}
-    @foreach ($courses as $course)
-        <a href="{{ url('course/' . $course->id) }}">
-            {{-- <img src="{{ Storage::url('Course Image/' . $course->course_image) }}"> --}}
-            <div>course : {{ $course->id }}</div>
-            <div>{{ $course->course_name }}</div>
-            <div>{{ $course->course_description }}</div>
-            <br>
-            <br>
-        </a>
-    @endforeach
+    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+        @foreach ($courses as $course)
+            <div class="carousel-inner">
+                <a href="{{ url('course/' . $course->id) }}">
+                    @if ($course == $courses[0])
+                        <div class="carousel-item active">
+                            <img src="img/Courses/DataStructures.jpg" class="d-block w-100" alt="Course-image">
+                            <div>{{ $course->course_name }}</div>
+                        </div>
+                    @else
+                        <div class="carousel-item">
+                            <img src="img/Courses/DataStructures.jpg" class="d-block w-100" alt="Course-image">
+                            <div>{{ $course->course_name }}</div>
+                        </div>
+                    @endif
+                </a>
+            </div>
+        @endforeach
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
 @endsection
