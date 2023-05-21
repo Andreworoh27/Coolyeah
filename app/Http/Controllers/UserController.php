159f41b1ca99d6342b2271aaa66bde023b3fbd38
Andrew Oroh <andreworoh.ao@gmail.com>
2023-05-21 11:10:22 +0700

    create register account feature

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\MessageBag;
 
 class UserController extends Controller
 {
@@ -25,6 +27,7 @@ class UserController extends Controller
     public function create()
     {
         //
+        return view('Authentication.register');
     }
 
     /**
@@ -36,6 +39,42 @@ class UserController extends Controller
     public function store(Request $request)
     {
         //
+        $this->validate($request, [
+            "username" => "required",
+            "email" => "required|unique:users,email",
+            "password" => "required | min:8 |alpha_num",
+            "gender" => "required",
+            "dob" => "required"
+        ]);
+
+        $dob = $request->dob;
+        if ($dob >= now()) {
+            return redirect()->back()->withErrors(new MessageBag(['Date of bith not valid']));
+        }
+
+        $username = $request->username;
+        $email = $request->email;
+        $password = $request->password;
+        $confirmpassword = $request->confirmpassword;
+        $telephone = $request->telephone;
+        $gender = $request->gender;
+
+        if ($password != $confirmpassword) {
+            return redirect()->back()->withErrors(new MessageBag(['Confirm password does not match the password']));
+        }
+
+        DB::table('users')->insert([
+            'name' => $username,
+            'email' => $email,
+            'gender' => $gender,
+            'dob' => $dob,
+            'password' => bcrypt($password),
+            'profile_image' => "default profile.png",
+            'role' => 'Member',
+            'created_at' => now(),
+        ]);
+
+        return redirect('login');
     }
 
     /**
