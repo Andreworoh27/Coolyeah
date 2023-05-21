072f0f09b13a4182ed1a8912abea31ec49c430a4
fakhrifend <fakhrifendiansyah@gmail.com>
2023-05-21 12:09:03 +0700

    adding styling for profile and course page

--- resources/views/Account/mycoursepage.blade.php
+++ resources/views/Account/mycoursepage.blade.php
@@ -3,11 +3,17 @@
 @section('title', 'My Course')
 
 @section('content')
-    @if ($usercourses == null)
-        <div>you have not subscribe to any course</div>
+<link rel="stylesheet" href="/css/myCourseStyle.css">
+<div class="outside-my-course">
+    @if ($usercourses -> isEmpty())
+        <div><h1>you have not subscribe to any course</h1></div>
     @else
         @foreach ($usercourses as $usercourse)
+        <div class="mycourse">
+            <img src="{{ Storage::url('img/Courses/' . $usercourse->course->course_image) }}" alt="Course-image">
+        </div>
             <div>{{ $usercourse->course->course_name }}</div>
         @endforeach
     @endif
+</div>
 @endsection
--- resources/views/Course/CourseDetail.blade.php
+++ resources/views/Course/CourseDetail.blade.php
@@ -3,25 +3,31 @@
 @section('title', 'Home')
 
 @section('content')
+<link rel="stylesheet" href="/css/courseDetailStyle.css">
     {{-- {{dd($CourseInfo)}} --}}
     @php
         $course = $CourseInfo[0];
         $subscribe = $CourseInfo[1];
     @endphp
-    <div>Course Detail page: </div>
-    {{-- <img src="{{ Storage::url('Course Image/' . $course->course_image) }}"> --}}
-    <div>{{ $course->id }}</div>
+    
+    <div class="outside-course-detail">
+    <div class="coursedetail">
+    <img src="{{ Storage::url('img/Courses/' . $course->course_image) }}" alt="Course-image">
+    </div>
     <div>{{ $course->course_name }}</div>
     <div>{{ $course->course_description }}</div>
     <div>{{ $course->course_rating }}</div>
+    <div class="subscribe">
     <div>{{ $course->course_subscriber }}</div>
     @if ($subscribe == false)
-        <a href="{{ url('course/subscribe/' . $course->id) }}">Subscribe Course</a>
+        <a href="{{ url('course/subscribe/' . $course->id) }}"><input type="submit" value="Subscribe course"></a>
     @else
         <form action="{{ url('usercourse/' . $course->id) }}" method="post">
             @csrf
             @method('DELETE')
             <input type="submit" value="Unsubscribe Course">
         </form>
+    </div>
     @endif
+</div>
 @endsection
