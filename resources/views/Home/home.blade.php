6fee7fb0df6ef087fa6d87f40d6d58697ae701db
StevennTio <steventio7b@gmail.com>
2023-06-18 21:44:50 +0700

    perbaikin display home

--- resources/views/Home/home.blade.php
+++ resources/views/Home/home.blade.php
@@ -26,7 +26,7 @@
                 </div>
                     <button class="carousel-control-prev" type="button" data-bs-target="#hero-carousel" data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
+                        <span class="visually-hidden" >Previous</span>
                     </button>
                     <button class="carousel-control-next" type="button" data-bs-target="#hero-carousel" data-bs-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
@@ -34,6 +34,7 @@
                     </button>
             </div>
         </div>
+        <div class="space"></div>
         <p>Popular</p>
     <div class="outside-course-list">
         <div class="container">
@@ -41,25 +42,23 @@
                 <a href="{{ url('course/' . $course->id) }}">
                     @if ($course->id < 3)
                         <div class="col">
-                            <div class="card radius-15">
-                                <div class="card-body text-center">
-                                    <div class="p-4 border radius-15">
-                                        <img src="{{ Storage::url('img/Courses/' . $course->course_image) }}"
-                                            class="d-block w-100" alt="Course-image">
-                                        <div class="course-name">{{ $course->course_name }}</div>
-                                        <div class="list-inline contacts-social mt-3 mb-3"> <a href="javascript:;"
-                                                class="list-inline-item bg-facebook text-white border-0"><i
-                                                    class="bx bxl-facebook"></i></a>
-                                            <a href="javascript:;"
-                                                class="list-inline-item bg-twitter text-white border-0"><i
-                                                    class="bx bxl-twitter"></i></a>
-                                            <a href="javascript:;"
-                                                class="list-inline-item bg-linkedin text-white border-0"><i
-                                                    class="bx bxl-linkedin"></i></a>
-                                        </div>
-                                        <div class="d-grid"> <a href="{{ url('course/' . $course->id) }}"
-                                                class="btn btn-outline-primary radius-15">Course detail</a>
-                                        </div>
+                            <div class="card radius-15" id="card1">
+                                <div class="p-4 border radius-15">
+                                    <img src="{{ Storage::url('img/Courses/' . $course->course_image) }}"
+                                        class="d-block w-100" alt="Course-image">
+                                    <div class="course-name">{{ $course->course_name }}</div>
+                                    <div class="list-inline contacts-social mt-3 mb-3"> <a href="javascript:;"
+                                            class="list-inline-item bg-facebook text-white border-0"><i
+                                                class="bx bxl-facebook"></i></a>
+                                        <a href="javascript:;"
+                                            class="list-inline-item bg-twitter text-white border-0"><i
+                                                class="bx bxl-twitter"></i></a>
+                                        <a href="javascript:;"
+                                            class="list-inline-item bg-linkedin text-white border-0"><i
+                                                class="bx bxl-linkedin"></i></a>
+                                    </div>
+                                    <div class="d-grid"> <a href="{{ url('course/' . $course->id) }}"
+                                            class="blue-button">Course detail</a>
                                     </div>
                                 </div>
                             </div>
